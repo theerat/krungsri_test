66d85838a7758cf4fbabe4517174ef07866e347c
Dev_Theerat <theerat56@outlook.com>
2019-12-20 15:36:25 +0700

    ีupdate master

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,20 @@ import React from 'react'
 import axios from 'axios'
 import { TextField, Grid, GridList, Link } from '@material-ui/core'
 import { UICard } from './common/MaterialUI';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+    Link: {
+
+        marginRight: 10
+    },
+    Form: {
+
+    },
+    Top10: {
+        marginTop: 10,
+    }
+};
 
 const BASE_URL = process.env.REACT_APP_API_ENDPOINT
 const APPID = '870de8f88ccba7aee687b1741eecc8fa'
@@ -11,12 +25,14 @@ class Home extends React.Component {
         super(props);
 
         this.state = {
-            city: '',
+            city: 'BANGKOK',
             units: 'metric',
+            description: '',
             weather_data: []
         };
 
         this.onChange = this.onChange.bind(this);
+        this.onhandleClick = this.onhandleClick.bind(this);
     }
 
     componentDidMount() {
@@ -28,9 +44,13 @@ class Home extends React.Component {
                 this.setState(
                     { weather_data: data }
                 );
+                this.getImageURL(data.weather[0].description)
             })
             .catch((err) => {
-                console.log(err);
+                this.setState({
+                    description: '',
+                    weather_data: []
+                });
             })
     }
 
@@ -44,18 +64,70 @@ class Home extends React.Component {
         console.log(url);
         await axios.get(url)
             .then(({ data }) => {
+                console.log('=== > data');
+                console.log(data);
                 this.setState({
                     weather_data: data
                 });
+
+                this.getImageURL(data.weather[0].description)
             })
             .catch((err) => {
+                console.log(err);
+                this.setState({
+                    description: '',
+                    weather_data: []
+                });
+            })
+    }
+
+    onhandleClick = async (e) => {
+
+        const name = e.target.name
+        const value = e.target.dataset.space
+
+        this.setState({
+            [name]: value,
+        });
+
+        const url = BASE_URL + `/weather?q=${this.state.city}&units=${value}&APPID=${APPID}`
+        console.log(url);
+        await axios.get(url)
+            .then(({ data }) => {
+                this.setState({
+                    weather_data: data
+                });
+                this.getImageURL(data.weather[0].description)
+            })
+            .catch((err) => {
+
                 console.log(err);
             })
     }
 
+    getImageURL = async (description) => {
+        let url = ''
+        if (description === 'few clouds') {
+            url = '//ssl.gstatic.com/onebox/weather/64/partly_cloudy.png'
+        }
+        else if (description === 'clear sky') {
+            url = '//ssl.gstatic.com/onebox/weather/48/sunny.png'
+        }
+        else if (description === 'heavy intensity rain' || description === 'rain' || description === 'moderate rain') {
+            url = '//ssl.gstatic.com/onebox/weather/48/rain.png'
+        }
+        else {
+            url = ''
+        }
+
+        this.setState({
+            description: url
+        });
+    }
 
 
     render() {
+        const { classes } = this.props;
         return (
             <div>
                 <h1>Weather Today</h1>
@@ -66,38 +138,33 @@ class Home extends React.Component {
                             name='city'
                             label="Input Your City"
                             onChange={this.onChange}
+
                         />
-                        <Link href="#" name='units' onClick={() => {
-                            this.setState({
-                                units: 'Metric'
-                            });
-                        }}>
+                    </Grid>
+                    <Grid xs={12} md={12} className={classes.Top10} >
+                        <Link className={classes.Link} variant="h4" data-space="Metric" value='Metric' href="#" name='units' onClick={this.onhandleClick}>
                             ํC
                         </Link>
-                        <Link href="#" name='units' onClick={() => {
-                            this.setState({
-                                units: 'Imperial'
-                            });
-                        }}>
+                        <Link className={classes.Link} variant="h4" data-space="Imperial" value='Imperial' href="#" name='units' onClick={this.onhandleClick}>
                             ํF
                         </Link>
                     </Grid>
-                    <GridList padding={20}>
-                        <Grid container
-                            direction="row"
-                            justify="center"
-                            alignItems="center"
-                            xs={12}
-                            md={12}>
-                            <UICard data={this.state}></UICard>
-                        </Grid>
-                    </GridList>
+                    <Grid container
+                        direction="row"
+                        justify="center"
+                        alignItems="center"
+                        xs={12}
+                        md={12}
+                        sm={12}
+                        className={classes.Top10}>
+                        <UICard data={this.state}></UICard>
+                    </Grid>
                 </form>
-            </div>
+            </div >
         )
     }
 
 
 }
 
-export default Home
+export default withStyles(styles)(Home)
--- src/components/common/MaterialUI.js
+++ src/components/common/MaterialUI.js
@@ -1,44 +1,38 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
+import { Card, Grid } from '@material-ui/core';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles({
-    card: {
-        minWidth: 275,
-    },
-    bullet: {
-        display: 'inline-block',
-        margin: '0 2px',
-        transform: 'scale(0.8)',
-    },
-    title: {
-        fontSize: 14,
-    },
-    pos: {
-        marginBottom: 20,
-    },
-});
-
 const UICard = (
     (props) => {
+        var cardStyle = {
+            display: 'block',
+            transitionDuration: '0.3s',
+        }
+
         const { data } = props
-        console.log(data);
         return (
-            <Card>
-                <CardContent>
-                    <Typography variant="h3" component="h2">
-                        {data.city}
-                    </Typography>
-                    <Typography variant="h4" component="p">
-                        {data.weather_data.main !== undefined ? data.weather_data.main['temp'] : ''} {data.units === 'Metric' ? ' ํC' : ' ํF'}
-                    </Typography>
-                </CardContent>
-            </Card>
+            <Grid>
+                <Card style={cardStyle}>
+                    <CardContent>
+                        <Typography variant="h3" component="h2">
+                            {<img src={data.description}></img>}
+                        </Typography>
+                        <Typography variant="h3" component="h2">
+                            {data.city}
+                        </Typography>
+                        <Typography variant="h4" component="p">
+
+                            อุณหภูมิ : {data.weather_data.main !== undefined ? data.weather_data.main['temp'] : ''} {data.units === 'Metric' ? ' ํC' : ' ํF'}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid >
         )
     }
 )
+
+
 
 export {
     UICard,
